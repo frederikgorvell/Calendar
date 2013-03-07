8667afc2b7c9058dc51b63dc32716cb9025c0198
Frederik de Lichtenberg <frederik_gorvell@hotmail.com>
2013-03-07 15:48:49 +0100

    First UserInterface

---
+++ src/ui/UserInterface.java
@@ -0,0 +1,21 @@
+package ui;
+
+import java.util.Scanner;
+
+public class UserInterface {
+	
+	public UserInterface() {
+		boolean loginOK = loginPrompt();
+	}
+	
+	private boolean loginPrompt() {
+		Scanner term = new Scanner(System.in);
+		System.out.println("Enter username: ");
+		String username = term.nextLine();
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
