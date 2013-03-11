6d6d5d5442521b8cca005d589523805c637f5c01
jonaha <Jonathan_h91@hotmail.com>
2013-03-11 15:57:46 +0100

    test

---
+++ src/client/model/User.java
@@ -0,0 +1,18 @@
+package client.model;
+
+public class User {
+	
+	private int id;
+	private String name;
+	private String email;
+	private int phonenumber;
+	
+	public User(String name, String email, int phonenumber) {
+		
+		this.name = name;
+		this.email = email;
+		this.phonenumber = phonenumber;
+		
+	}
+
+}
