7d87c3336457fc90ee5d5a66756ffcda1f151842
jonaha <Jonathan_h91@hotmail.com>
2013-03-12 10:36:53 +0100

    user class

--- src/client/model/User.java
+++ src/client/model/User.java
@@ -16,11 +16,35 @@ public class User {
 	}
 	
 	public void createProfile() {
-		//
+		
 	}
 	
 	public void editProfile() {
 		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(int phonenumber) {
+		this.phonenumber = phonenumber;
 	}
 
 }
