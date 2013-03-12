dcd608a4566437332ad3823fd6f430305c8ad50b
jonaha <Jonathan_h91@hotmail.com>
2013-03-12 11:32:56 +0100

    added fields to user

--- src/client/model/User.java
+++ src/client/model/User.java
@@ -1,26 +1,28 @@
 package client.model;
 
+
 public class User {
 	
-	private int id;
+	private final int id;
 	private String name;
 	private String email;
 	private int phonenumber;
+	private String username;
+	private String password;
+	XMLConverter converter = new XMLConverter();
 	
-	public User(String name, String email, int phonenumber) {
-		
+	public User(int id, String name, String email, int phonenumber, String username, String password) {	
+		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.phonenumber = phonenumber;
-		
+		this.username = username;
+		this.password = password;
+		converter.toXML(this);
 	}
 	
-	public void createProfile() {
-		
-	}
-	
-	public void editProfile() {
-		
+	public int getId() {
+		return id;
 	}
 
 	public String getName() {
@@ -45,6 +47,22 @@ public class User {
 
 	public void setPhonenumber(int phonenumber) {
 		this.phonenumber = phonenumber;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 }
